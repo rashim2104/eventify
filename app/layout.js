f084eb281374ed182b6cc98cc2d2d67da9dd7f84
Rashim <rashimrb22@gmail.com>
2025-01-02 11:39:37 +0530

    feat: add animated background and enhance SEO metadata for homepage

--- app/layout.js
+++ app/layout.js
@@ -5,30 +5,57 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar/navbar";
 import Sidebar from "@/components/Sidebar/sidebar";
 import { Toaster } from "sonner";
-// import { useState , useEffect } from "react";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Eventify",
-  description: "Manage all your events",
-};
+  title: 'Eventify',
+  description: 'The official event management platform for organizing and tracking academic events.',
+  keywords: 'eventify sairam, sairam events, sairam college events, sri sairam engineering college events, event management system, college event platform',
+  openGraph: {
+    title: 'Eventify',
+    description: 'The official event management platform for organizing and tracking academic events.',
+    images: ['/assets/images/logo-main.png'],
+    url: 'https://eventify.sairam.edu.in',
+    siteName: 'Eventify Sairam',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+  }
+}
 
 export default function RootLayout({ children }) {
-  // const [showToaster, setShowToaster] = useState(false);
-
-  // useEffect(() => {
-  //   if (showToaster) {
-  //     const timerId = setTimeout(() => {
-  //       setShowToaster(false);
-  //     }, 2000);
-
-  //     return () => clearTimeout(timerId);
-  //   }
-  // }, [showToaster]);
 
   return (
     <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-ZC06WK8GTF`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZC06WK8GTF');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Toaster richColors position="top-right" closeButton/>
         <AuthProvider>
